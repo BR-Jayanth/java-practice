d5353cd70e0fc0c154ff0a821977e8b28b21791a
BR-Jayanth <jayanthbychana@gmail.com>
2024-04-15 10:58:26 +0530

    class 3 on arrays

---
+++ fullstack/back-end/languages/java/core_java/src/dataTypes/MyArrays.java
@@ -0,0 +1,22 @@
+package dataTypes;
+
+public class MyArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="apple";
+		String b="ball";
+		String c="cat";
+		System.out.println(a);
+		
+		
+		String[] all= {"apple","ball","cat","dog"};
+		System.out.println(all);
+		System.out.println(all[0]);
+		for(int i=0;i<all.length;i++) {
+			System.out.println(all[i]);
+		}
+		
+	}
+
+}
---
+++ fullstack/back-end/languages/java/core_java/src/dataTypes/Numbers.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num= 1234;
+		
+//		Integer num2=1234567890;
+		double num1=12345678901d;
+		float dec=123.4f;
+		
+		System.out.println(num);
+	}
+
+}
